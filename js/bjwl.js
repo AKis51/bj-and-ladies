59b31545a085ba8796b33bea05392026d5bfb1fb
AKis51 <akisenkov@gmail.com>
2016-09-28 15:11:37 +0300

    Added function getDeck()

--- js/bjwl.js
+++ js/bjwl.js
@@ -33,3 +33,26 @@ function getCard(deck) {
 }
 
 */
+
+function getDeck(deck_size) {
+    "use strict";
+    var deck;
+
+    if (deck_size === 36) {
+        deck = ["cA", "c6", "c7", "c8", "c9", "c10", "cJ", "cQ", "cK",
+                "dA", "d6", "d7", "d8", "d9", "d10", "dJ", "dQ", "dK",
+                "hA", "h6", "h7", "h8", "h9", "h10", "hJ", "hQ", "hK",
+                "sA", "s6", "s7", "s8", "s9", "s10", "sJ", "sQ", "sK"];
+        return deck;
+    }
+
+    if (deck_size === 52) {
+        deck = ["cA", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "cJ", "cQ", "cK",
+                "dA", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "dJ", "dQ", "dK",
+                "hA", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "hJ", "hQ", "hK",
+                "sA", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "sJ", "sQ", "sK"];
+        return deck;
+    }
+}
+
+alert(getDeck(36));
